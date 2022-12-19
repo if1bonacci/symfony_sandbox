a9d0070711b427170e79066a7a01e6a5e471c46e
Yevhen.E <kea.riland@gmail.com>
2022-12-19 17:27:02 +0200

    init commit

---
+++ tests/Service/NewsletterGeneratorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Service;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class NewsletterGeneratorTest extends KernelTestCase
+{
+    public function testSomething()
+    {
+        self::bootKernel();
+
+    }
+}
